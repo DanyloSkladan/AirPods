02fa0ac8cba25ac92b181d3cb85fa0d348f26044
DanyloSkladan <dskladannnnn09082001@gmail.com>
2023-11-19 22:28:37 +0300

    color

--- js/main.js
+++ js/main.js
@@ -6,27 +6,26 @@ chooseColor.forEach(function(element){
     element.addEventListener('click', open)
 })
 
-function open(evt) {
+function open(evt){
 
 const target = evt.currentTarget;
 const button = target.dataset.button;
-const contentActive = document.querySelectorAll(`.${button}`)
+const contentActive = document.querySelectorAll(`.${button}`) 
 
-chooseColor.forEach(function(item) {
+chooseColor.forEach(function(item){
     item.classList.remove('choose__color-btn--active')
 })
 
 
-target.classList.add ('choose__color-btn--active')
+target.classList.add('choose__color-btn--active')
 
 contentItem.forEach(function(item){
     item.classList.remove('content-item__active')
 })
 
-contentItem.forEach(function(item){
-    item.classList.remove('choose__color-btn--active')
+contentActive.forEach(function(item){
+    item.classList.add('content-item__active')
 })
 
-target.classList.add('choose__color-btn--actiev')
 
 }
